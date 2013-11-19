5deac0d3ed889150e04ea9277955c3c5a17e7e45
Knut Mork <knut.mork@gmail.com>
2013-11-19 19:58:23 +0100

    Added mongohq backend

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,19 +1,33 @@
 package controllers
 
 import play.api.mvc._
+import com.mongodb.casbah.Imports._
+import play.api.Play
+import play.api.Play.current
 
 object Application extends Controller {
+
+  val uri = MongoClientURI(Play.configuration.getString("mongodb.uri").get);
+  val mongoClient = MongoClient(uri);
+  val db = mongoClient("matmaps");
+  val coll = db("map");
 
   def index = Action {
     Ok(views.html.index())
   }
 
   def newMap = Action {
-    Redirect(routes.Application.getMap(RandomString.createRandomString(16)));
+    var mongoDoc : MongoDBObject = null;
+    var mapId : String = null;
+    do {
+      mapId = RandomString.createRandomString(16);
+      mongoDoc = MongoDBObject("id" -> mapId);
+    } while (coll.findOne(mongoDoc) isDefined);
+    coll.insert( mongoDoc );
+    Redirect(routes.Application.getMap(mapId));
   }
 
   def getMap(id: String) = Action {
     Ok(views.html.newMap());
   }
-
 }
--- build.sbt
+++ build.sbt
@@ -3,9 +3,10 @@ name := "mat"
 version := "1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
-  jdbc,
   anorm,
-  cache
+  cache,
+  "org.mongodb" %% "casbah" % "2.6.3"
 )     
 
 play.Project.playScalaSettings
+
